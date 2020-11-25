7212a588c9fb088dd89b0e7740ef35b36428245a
sreedhanya <sreedhanya.r1998@gmail.com>
2020-11-25 09:20:47 +0530

    listprograms committed

---
+++ pythondatastructure/listprograms/listprogram.py
@@ -0,0 +1,22 @@
+#name=['ajay','vijay','john','sijo',10,10.0,True]
+#print(name)
+
+
+list=['java','python','c','javascript']
+print(list)
+print(list[0])
+print(list[3])
+print(list[-1])
+print(list[-1:-5:-1])
+print(list[0:4:2])
+print(list[:3:2])
+for item in list:
+    print(item)
+list.append("dart")
+print(list)
+list[0]="ruby"
+print(list)
+list.append("python")
+print(list)
+list.insert(3,"perl")
+print(list)
